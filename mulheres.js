92213d9ce9c3c65221ddfd928f4681cc5b95bfb7
alinealves99 <alinealves_99@hotmail.com>
2025-04-12 12:24:26 -0300

    Atualiza a função de cadastro de mulheres

--- mulheres.js
+++ mulheres.js
@@ -1,20 +1,24 @@
-const express = require("express");
-const app = express();
-const porta = 3333;
-const router = express.Router();
-
+const express = require("express"); //iniciando o express
+const app = express(); //iniciando o app
+const porta = 3333; // criando a porta
+const router = express.Router();// configurando a primeira parte da rota
+const { v4:uuidv4} = require('uuid');
+//Lista inicial de mulheres
 const mulheres = [
     {
+        id:'1',
         nome: 'Aline Ribeiro',
         imagem:'https://media.licdn.com/dms/image/v2/C4D03AQGdmzDr-6VsQw/profile-displayphoto-shrink_800_800/profile-displayphoto-shrink_800_800/0/1628692640734?e=1747267200&v=beta&t=Uos1vXN_52lIBnstQL6JoELt-3lQ_gTrqyRCCJddx1A',
         minibio: 'Desenvolvedora front-end'
     },
     {
+        id:'2',
         nome: 'Aline Ribeiro',
         imagem:'https://media.licdn.com/dms/image/v2/C4D03AQGdmzDr-6VsQw/profile-displayphoto-shrink_800_800/profile-displayphoto-shrink_800_800/0/1628692640734?e=1747267200&v=beta&t=Uos1vXN_52lIBnstQL6JoELt-3lQ_gTrqyRCCJddx1A',
         minibio: 'Desenvolvedora front-end'
     },
     {
+        id:'3',
         nome: 'Aline Ribeiro',
         imagem:'https://media.licdn.com/dms/image/v2/C4D03AQGdmzDr-6VsQw/profile-displayphoto-shrink_800_800/profile-displayphoto-shrink_800_800/0/1628692640734?e=1747267200&v=beta&t=Uos1vXN_52lIBnstQL6JoELt-3lQ_gTrqyRCCJddx1A',
         minibio: 'Desenvolvedora front-end'
@@ -26,8 +30,26 @@ function mostraPorta(){
     console.log("Servidor",porta);
 
 }
+//GET
 function mostraMulheres(request,response){
     response.json(mulheres);
 }
-app.listen(porta,mostraPorta);
-app.use(router.get('/mulheres',mostraMulheres));
+
+//POST
+function criaMulher (request,response){
+    const novaMulher ={
+    id:uuidv4(),
+    nome:request.body.nome,
+    imagem:request.body.imagem,
+    minibio:request.body.minibio
+}
+
+mulheres.push(novaMulher)
+
+response.json(mulheres)
+
+}
+
+app.listen(porta,mostraPorta);// Servidor ouvindo a porta
+app.use(router.post('/mulheres', criaMulher)) // configuração rota POST /mulheres
+app.use(router.get('/mulheres',mostraMulheres));// configuração rota GET /mulheres
