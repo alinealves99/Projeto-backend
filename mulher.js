6e71632b8439dd6a05e495c59ed02ebf235d6549
Aline Ribeiro Alves <138813866+alinealves99@users.noreply.github.com>
2025-04-23 18:15:37 -0300

    Update mulher.js

--- mulher.js
+++ mulher.js
@@ -6,7 +6,7 @@ const router = express.Router();
 function mostraMulher(request,response){
     response.json({
         nome: 'Aline Ribeiro',
-        imagem:'https://media.licdn.com/dms/image/v2/C4D03AQGdmzDr-6VsQw/profile-displayphoto-shrink_800_800/profile-displayphoto-shrink_800_800/0/1628692640734?e=1747267200&v=beta&t=Uos1vXN_52lIBnstQL6JoELt-3lQ_gTrqyRCCJddx1A',
+        imagem:'',
         minibio: 'Desenvolvedora front-end'
     })
 }
